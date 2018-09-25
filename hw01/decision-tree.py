f6a1e3fdf2d1b34f4dd5b9f72f1d046fed7ac46e
Zach <caday33@gmail.com>
2018-09-24 19:54:16 -0500

    partial fix to debug_data format

--- hw01/decision-tree.py
+++ hw01/decision-tree.py
@@ -28,16 +28,14 @@ class Mushroom:
         self.isEdible = arr[21]
 
     def debug_data(self):
-        return '\n ~~~~~Mushroom~~~~~ \n Cap_Shape: {}, Cap_Surface: {}, Cap_Color: {}, '.format(self.cap_shape, self.cap_surface, self.cap_color)
-            + 'Bruises: {}, Odor: {}, '.format(self.bruises, self.odor)
-            + 'Gill_Attach: {}, Gill_Space: {}, Gill_Size: {}, Gill_Color: {}, '.format(self.gill_attachment, self.gill_spacing, self.gill_size, self.gill_color)
-            + 'Stalk_Shape: {}, Stalk_Root: {}, '.format(self.stalk_shape, self.stalk_root)
-            + 'Stalk_Surface_A: {}, Stalk_Surface_B: {}, Stalk_Color_A: {}, Stalk_Color_B: {}, '.format(self.stalk_surface_above_ring, self.stalk_surface_below_ring, self.stalk_color_above_ring, self.stalk_color_below_ring)
-            + 'Veil_Type: {}, Veil_Color: {}, '.format(self.veil_type, self.veil_color)
-            + 'Ring_Number: {}, Ring_Type: {}, '.format(self.ring_number, self.ring_type)
-            + 'Spore_P_Color: {}, Population: {}, '.format(self.spore_print_color, self.population)
-            + 'Habitat: {}, Edible/Posion: {}'.format(self.habitat, self.isEdible)
-            + '\n ~~~~~~~~~~~~~~~~~~ \n'
+        return '\n ~~~~~Mushroom~~~~~ \n Cap_Shape: ' + self.cap_shape + ', Cap_Surface: ' + self.cap_surface + ', Cap_Color: ' + self.cap_color + ', '
+            + 'Bruises: ' + self.bruises + ', Odor: ' + self.odor + ', Gill_Attach: ' + self.gill_attachment + ', Gill_Space: ' + self.gill_spacing + ', '
+            + 'Gill_Size: ' + self.gill_size + ', Gill_Color: ' + self.gill_color + ', Stalk_Shape: ' + self.stalk_shape + ', Stalk_Root: ' + self.stalk_root + ', '
+            + 'Stalk_Surface_A: ' + self.stalk_surface_above_ring + ', Stalk_Surface_B: ' + self.stalk_surface_below_ring + ', ' 
+            + 'Stalk_Color_A: ' + self.stalk_color_above_ring + ', Stalk_Color_B: ' + self.stalk_color_below_ring + ', '
+            + 'Veil_Type: ' + self.veil_type + ', Veil_Color: ' + self.veil_color + ', Ring_Number: ' + self.ring_number + ', Ring_Type: ' + self.ring_type + ', '
+            + 'Spore_P_Color: ' + self.spore_print_color + ', Population: ' + self.population + ', '
+            + 'Habitat: ' + self.habitat + ', Edible/Posion: ' + self.isEdible + '\n ~~~~~~~~~~~~~~~~~~ \n'
 
 
 
